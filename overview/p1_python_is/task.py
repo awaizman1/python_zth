2d50f4a3fd217edb84c491537636558849da5aae
awaizman1 <awaizman@gmail.com>
2018-10-02 22:42:54 +0300

    mv

--- overview/p1_python_is/task.py
+++ overview/p1_python_is/task.py
@@ -2,13 +2,19 @@
 # --- python is strongly typed
 # lets verify that every object has a definite type
 for obj in [1, True, 2.5, "hello", [1, 2, 3], {1: "one", 2: "two"}]:
-    print(type(obj))
+    # your implementation here
+    # hints:
+    # 1. print is your buddy
+    # 2. https://docs.python.org/3/library/functions.html#type
+
 
 
 # --- python is dynamically typed - adding new code
 # can you change the code so that 'new_code' will be added and executed?
 new_code = "print('where the hell this line came from?')"
-eval(new_code)
+# your implementation here
+# hints:
+# 1. https://docs.python.org/3/library/functions.html#eval
 
 
 # --- python is dynamically typed - extend objects
@@ -20,6 +26,8 @@ class Student:
 
 
 student = Student("jhon", 27)
-student.gender = 'male'
+# your implementation here
+# hints:
+# 1. setting an object attribute is similar to accessing it
 
 print("student %s is %s" % (student.name, student.gender))
