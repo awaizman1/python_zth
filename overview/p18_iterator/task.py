58192cf7c1d88290a3a359f02cc220b54389d6ec
awaizman1 <awaizman@gmail.com>
2019-01-11 16:31:23 +0200

    Create task.py

---
+++ overview/p18_iterator/task.py
@@ -0,0 +1,21 @@
+# StudentGrade is a simple class holding a sequence of students and an associated
+# sequence of grades.
+# 1. make StudentGrade iterable.
+# 2. make sure it can iterated multiple times.
+
+
+class StudentGrade:
+    def __init__(self, students, grades):
+        self._students = students
+        self._grades = grades
+
+    raise NotImplementedError("YOUR IMPLEMENTATION HERE")
+
+
+student_grades = StudentGrade(["Dave", "Shirley", "Lena", "Jhon"], [78, 95, 92, 56])
+for student, grade in student_grades:
+    print(student, ": ", grade)
+
+# make sure we can iterate it again
+for student, grade in student_grades:
+    print(student, ": ", grade)
